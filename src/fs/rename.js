4c3b3d533ca39df55ee0b4891b42e1fb1f68638a
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-01-24 14:23:04 +0100

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const renamedFile = './src/fs/files/wrongFilename.txt';
+    const newFileName = './src/fs/files/properFilename.md';
+
+    try {
+        if(!fs.existsSync(renamedFile) || fs.existsSync(newFileName)) {
+            throw new Error('FS operation failed')
+        }
+
+        fs.renameSync(renamedFile, newFileName)
+    } catch (err) {
+        throw err
+    }
 };
 
 await rename();
