8198733229357625b009500bd5eb3ee7721e44e2
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-01-24 14:36:47 +0100

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { promises as fsPromises } from 'fs';
+
 const list = async () => {
-    // Write your code here 
+  const folderPathName = './src/fs/files';
+  try {
+    if (!fsPromises.access(folderPathName)) {
+      throw new Error('FS operation failed');
+    }
+
+    const files = await fsPromises.readdir(folderPathName);
+
+    console.log('Array with files', files);
+  } catch (err) {
+    throw err;
+  }
 };
 
 await list();
