161613d65f49c67fa78bc70271fe720eda3ed6bc
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-01-24 12:20:15 +0100

    feat: implement create function and add created file fresh

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = './src/fs/files/fresh.txt';
+  if (fs.existsSync(filePath)) {
+    throw new Error('FS operation failed');
+  }
+  
+  fs.appendFile(filePath, 'I am fresh and young', (error) => {
+    if (error) throw error;
+    console.log('Saved!');
+  });
 };
 
 await create();
