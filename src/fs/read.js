433a2bc48b459c5e344108da928fcef6dc09f717
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-01-24 14:50:55 +0100

    feat: implement read function logic

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,34 @@
+import { promises as fsPromises } from 'fs';
+
+const doesFileExist = async (path) => {
+  try {
+    await fsPromises.access(path);
+
+    return true;
+  } catch (err) {
+    if ((err.code = 'ENOENT')) {
+      return false;
+    } else {
+      throw err;
+    }
+  }
+};
+
 const read = async () => {
-    // Write your code here 
+  const pathToFile = './src/fs/files/fileToRead.txt';
+  try {
+    const isFileExist = await doesFileExist(pathToFile);
+
+    if (!isFileExist) {
+      throw new Error('FS operation failed');
+    }
+
+    const content = await fsPromises.readFile(pathToFile, 'utf-8');
+
+    console.log(content);
+  } catch (err) {
+    throw err;
+  }
 };
 
 await read();
