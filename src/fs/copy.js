42b74d87aac4f833f3fd572714f25a4a38dc99b9
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-01-24 14:08:07 +0100

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+  const copyFolderPath = './src/fs/files_copy';
+  const sourceFolderPath = './src/fs/files';
+
+  try {
+    if (!fs.existsSync(sourceFolderPath) || fs.existsSync(copyFolderPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.cpSync(sourceFolderPath, copyFolderPath, {
+      recursive: true,
+    });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await copy();
