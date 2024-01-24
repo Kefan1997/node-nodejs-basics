2ce76e7e1aef69394b7e9efd6a473c3e5c349d66
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-01-24 14:28:02 +0100

    feat: implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    const pathToDeleteFile = './src/fs/files/fileToRemove.txt';
+
+    try {
+        if(!fs.existsSync(pathToDeleteFile)) {
+            throw new Error('FS operation failed')
+        }
+
+        fs.unlinkSync(pathToDeleteFile);
+    } catch (err) {
+        throw err
+    }
 };
 
 await remove();
