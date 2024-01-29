3b2b8089c0055236b8640c05c4c904ac256f2bb2
OlegRashchinsky <oleg.rashinskiy@gmail.com>
2024-01-29 19:39:42 +0100

    feat: implement function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'fs';
+
+import { getPath } from '../helpers/index.js';
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = 'fileToRead.txt';
+  const folderName = 'files';
+
+  const pathToFileToRead = getPath(import.meta.url, folderName, fileToRead);
+
+  const readableStream = createReadStream(pathToFileToRead, { encoding: 'utf8' });
+
+  readableStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readableStream.on('end', () => {
+    console.log('\nFile reading completed.');
+  });
+
+  readableStream.on('error', (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  });
 };
 
 await read();
