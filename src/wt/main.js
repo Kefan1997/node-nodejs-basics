e2af5b964ed94a079ae586f583f2e9503359f46d
OlegRashchinsky <oleg.rashinskiy@gmail.com>
2024-01-29 20:11:22 +0100

    feat: implement Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import * as os from 'os';
+import { Worker } from 'worker_threads';
+
+import { getPath } from '../helpers/index.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const fileToRead = 'worker.js';
+  const cpus = os.cpus();
+  const pathToFile = getPath(import.meta.url, fileToRead);
+  const result = [];
+
+  cpus.map((_, i) => {
+    const worker = new Worker(pathToFile);
+    worker.on('message', (res) => {
+      result.push(res);
+
+      result.length === cpus.length && console.log(result);
+    });
+
+    worker.postMessage(i + 10);
+  });
 };
 
 await performCalculations();
