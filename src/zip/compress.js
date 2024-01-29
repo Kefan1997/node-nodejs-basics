e9d68a47b78394ad9a7d6a9f8afebf357e2c3af4
OlegRashchinsky <oleg.rashinskiy@gmail.com>
2024-01-29 19:23:36 +0100

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+import { getPath } from '../helpers/index.js';
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = 'fileToCompress.txt';
+  const compressedFileName = 'archive.gz';
+  const folderName = 'files';
+
+  const pathToTheFileCompress = getPath(import.meta.url, folderName, fileToCompress);
+  const pathToTheCompressedFileName = getPath(import.meta.url, folderName, compressedFileName);
+
+  const input = createReadStream(pathToTheFileCompress);
+  const output = createWriteStream(pathToTheCompressedFileName);
+  const gzip = createGzip();
+
+  input.pipe(gzip).pipe(output)
+
+  output.on('finish', () => {
+    console.log(`File ${fileToCompress} compressed to ${compressedFileName}`);
+  });
+
+  output.on('error', (error) => {
+    console.error(`Error compressing file: ${error.message}`);
+  });
 };
 
 await compress();
