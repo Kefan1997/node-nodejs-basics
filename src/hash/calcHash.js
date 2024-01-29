d77163afcae2d19d8a11bf31d972f0f2d75fbcad
OlegRashchinsky <oleg.rashinskiy@gmail.com>
2024-01-29 18:48:48 +0100

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
+import { getPath } from '../helpers/index.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileToCalculateHashFor = 'fileToCalculateHashFor.txt';
+  const sha256 = 'sha256';
+
+  const pathToTheFile = getPath(import.meta.url, folderName, fileToCalculateHashFor);
+
+  const hash = createHash(sha256);
+  const stream = createReadStream(pathToTheFile);
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    const sha256Hash = hash.digest('hex');
+    console.log(`SHA256 Hash for ${fileToCalculateHashFor}: ${sha256Hash}`);
+  });
+
+  stream.on('error', (error) => {
+    console.error(`Error reading file: ${error.message}`);
+  });
 };
 
 await calculateHash();
