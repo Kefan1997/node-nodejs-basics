220a007ba5b0e9df873f76f560bd8706e81b5533
OlegRashchinsky <oleg.rashinskiy@gmail.com>
2024-01-29 19:57:44 +0100

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,30 @@
+import { Transform } from 'stream';
+
+class ReverseTransform extends Transform {
+    _transform(chunk, encoding, callback) {
+        const reversedChunk = chunk.toString().split('').reverse().join('');
+
+        this.push(reversedChunk);
+        callback();
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new ReverseTransform();
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+    process.stdin.on('end', () => {
+        console.log('\nText reversal completed.')
+    })
+
+    process.stdin.on('error', (error) => {
+        console.error(`Error reading from stdin: ${error.message}`);
+      });
+    
+      process.stdout.on('error', (error) => {
+        console.error(`Error writing to stdout: ${error.message}`);
+      });
 };
 
 await transform();
