70ed172ae19d96e5d822adfa34d19f67d37f3f84
OlegRashchinsky <oleg.rashinskiy@gmail.com>
2024-01-29 19:53:05 +0100

    feat: implemented write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,35 @@
+import { createWriteStream } from 'fs';
+
+import { getPath } from '../helpers/index.js';
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = 'fileToWrite.txt';
+  const folderName = 'files';
+
+  const pathToFileToWrite = getPath(import.meta.url, folderName, fileToWrite);
+
+  const writableStream = createWriteStream(pathToFileToWrite, { encoding: 'utf8' });
+
+  process.stdin.setEncoding('utf-8');
+
+  process.stdin.on('data', (chunk) => {
+    if (chunk !== null) {
+      writableStream.write(chunk);
+    }
+  });
+
+  process.stdin.on('end', () => {
+    writableStream.end();
+    console.log(`Data written to ${fileToWrite}`);
+  });
+
+  process.stdin.on('error', (error) => {
+    console.error(`Error reading from stdin: ${error.message}`);
+  });
+
+  writableStream.on('error', (error) => {
+    console.error(`Error writing to file: ${error.message}`);
+  });
 };
 
 await write();
