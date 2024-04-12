bf791e31cb40cc30742e3f164a642ae0381ad630
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-04-12 14:47:42 +0200

    feat: add create implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { writeFile } from 'node:fs';
+
+import { getPath, doesFileExist, operationFail } from '../helpers/index.js';
+
 const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const path = getPath(import.meta.url, 'files', 'fresh.txt');
+
+  const fileExist = await doesFileExist(path);
+
+  if (fileExist) {
+    operationFail();
+
+    return;
+  }
+
+  await writeFile(path, content, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('File written successfully');
+    }
+  });
 };
 
 await create();
