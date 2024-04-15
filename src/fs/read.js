bd5bedebea6cdf931f9be8423d2ccc67be666265
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-04-15 10:37:30 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'node:fs/promises';
+
+import { getPath, doesPathExist, operationFail } from '../helpers/index.js';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = getPath(import.meta.url, 'files', 'fileToRead.txt');
+  const isExist = doesPathExist(filePath);
+
+  if (!isExist) {
+    operationFail();
+
+    return;
+  }
+
+  const content = await readFile(filePath, { encoding: 'utf8' });
+  console.log(content);
 };
 
 await read();
