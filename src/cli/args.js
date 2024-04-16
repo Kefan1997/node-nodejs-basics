68d4439da439ce6806a0c7460c0db96ba3ca1ba9
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-04-16 09:30:58 +0200

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const res = [];
+
+  for (let i = 0; i < args.length; i = i + 2) {
+    const propName = args[i].substring(2);
+    const value = args[i + 1];
+
+    res.push(`${propName} is ${value}`);
+  }
+
+  console.log(res.join(', '));
 };
 
 parseArgs();
