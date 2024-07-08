3b0879a12c4c717f4cc30a4f09795d56b5e88318
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-07-08 09:51:28 +0200

    feat: implement write function by stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from 'node:fs';
+import { createWriteStream } from 'node:fs';
+import { finished } from 'node:stream/promises';
+
+import { getPath } from '../helpers/index.js';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = getPath(import.meta.url, 'files', 'fileToWrite.txt');
+
+  const writableStream = createWriteStream(filePath, { encoding: 'utf8' });
+
+  try {
+    process.stdin.pipe(writableStream);
+
+    await finished(writableStream);
+
+    console.log('\nFinished writable file');
+  } catch (err) {
+    console.error('An error occurred:', err.message);
+  }
 };
 
 await write();
