ccbbca4ee04f488d5851900be704de783c9d0140
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-07-08 09:51:13 +0200

    feat: implement read function by stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+import { finished } from 'node:stream/promises';
+
+import { getPath } from '../helpers/index.js';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = getPath(import.meta.url, 'files', 'fileToRead.txt');
+
+  const readableStream = createReadStream(filePath, { encoding: 'utf8' });
+
+  try {
+    readableStream.pipe(process.stdout);
+
+    await finished(readableStream);
+
+    console.log('\nFinished reading file');
+  } catch (err) {
+    console.error('An error occurred:', err.message);
+  }
 };
 
 await read();
