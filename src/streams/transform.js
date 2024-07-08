8415e65059aa2bbb25a56b6ef85e7656a65cd951
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-07-08 09:51:52 +0200

    feat: implement transform function by stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        this.push(chunk.toString().split('').reverse().join(''));
+        callback();
+      },
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+  } catch (err) {
+    console.error('An error occurred:', err.message);
+  }
 };
 
 await transform();
