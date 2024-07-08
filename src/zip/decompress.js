28a4c582b170bf8e9279cd009191b4f5e2d0380f
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-07-08 17:45:52 +0200

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'stream/promises';
+
+import { getPath } from '../helpers/index.js';
+
 const decompress = async () => {
-    // Write your code here 
+  const archiveFilePath = getPath(import.meta.url, 'files', 'archive.gz');
+  const decompressFilePath = getPath(import.meta.url, 'files', 'fileToCompressNew.txt');
+
+  const gzip = createGunzip();
+  const source = createReadStream(archiveFilePath);
+  const destination = createWriteStream(decompressFilePath);
+
+  try {
+    await pipeline(source, gzip, destination);
+
+    console.log('File successfully decompressed to archive.gz');
+  } catch (err) {
+    console.error('An error occurred:', err.message);
+  }
 };
 
 await decompress();
