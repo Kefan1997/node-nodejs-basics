9433ce2cdbd97ec78d1f988f3f1f944f98ec3951
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2024-07-09 09:30:40 +0200

    fix: use the built-in module node:stream

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,6 +1,6 @@
 import { createGzip } from 'node:zlib';
 import { createReadStream, createWriteStream } from 'node:fs';
-import { pipeline } from 'stream/promises';
+import { pipeline } from 'node:stream/promises';
 
 import { getPath } from '../helpers/index.js';
 
